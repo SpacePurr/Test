b8c0781157523ad51f9b380f4454d0e60b60bc59
SpacePurr <43045833+SpacePurr@users.noreply.github.com>
2020-08-04 22:19:11 +0300

    Update create_table.sql

--- MS_SQL_DataBase/create_table.sql
+++ MS_SQL_DataBase/create_table.sql
@@ -3,5 +3,5 @@ create table quotes
 	id bigint not null identity(1, 1) primary key,
 	id_package bigint not null,
 	package bigint not null,
-	date datetime not null
+	send_date datetime not null
 )
